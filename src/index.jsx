dd1ee6567f913fd4c135b5175d262bf2476bd98a
Tasneem <tasneem123@gmail.com>
2026-06-13 00:43:24 +0530

    updated

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
-import AppDetail from "./components/AppDetails";
+import AppDetail from "./components/AppDetail";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
-        <Route path="/apps/:id" element={<AppDetails/>}
+        <Route path="/apps/:id" element={<AppDetail/>}
 />
       </Routes>
     </BrowserRouter>
